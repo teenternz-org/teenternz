15c62707c1b96feb2c0bc49aff6b1fc5712d0859
tejasc0 <tejasc888@gmail.com>
2021-09-03 07:30:37 +0000

    fixes

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -18,12 +18,12 @@ class Navbar extends Component {
         <div className="text-white text-2xl cursor-pointer"><Link href="/"><a>Teenternz</a></Link></div>
         <div className="text-white p-2 cursor-pointer menu-icon" onClick={this.handleClick}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-</svg></div>
+</svg></div> 
         <ul className={this.state.active ? 'nav-menu active' : 'nav-menu'}>
-        <div className="text-black text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/internships"><a>Internships</a></Link></div>
-        <div className="text-black mt-0 text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/events"><a>Events</a></Link></div>
-        <div className="text-black text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/articles"><a>Articles</a></Link></div>
-        <div className="text-black text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/about"><a>About</a></Link></div>
+        <div className="text-black text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/internships"><a onClick={this.handleClick}>Internships</a></Link></div>
+        <div className="text-black mt-0 text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/events"><a onClick={this.handleClick}>Events</a></Link></div>
+        <div className="text-black text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/articles"><a onClick={this.handleClick}>Articles</a></Link></div>
+        <div className="text-black text-2xl p-3 hover:bg-purple-200 w-full cursor-pointer"><Link href="/about"><a onClick={this.handleClick}>About</a></Link></div>
         </ul>
        
       </nav>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
 
 /* Vanilla JS imports */
-import '../styles/vanilla-js/style.css'
-import '../styles/vanilla-js/internships.css'
+import '../styles/style.css'
 import Layout from '../components/layout'
+import '../styles/internships.css'
 
 function MyApp({ Component, pageProps }) {
   return (

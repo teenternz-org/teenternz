f189a14f5943fd563796ffbb9bd7a58da15af2f2
tejasc0 <tejasc888@gmail.com>
2021-09-05 07:41:44 +0000

    fixes

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -21,7 +21,7 @@ class Navbar extends Component {
         <Link href="#" passHref><div className="text-lg font-medium p-2 cursor-pointer">Log In</div></Link>
         <Link href="#" passHref><div className="text-lg py-2 px-4 shadow-md cursor-pointer rounded-lg font-medium bg-blue-500 text-white">Join</div></Link>
         </div>
-        <div className="text-black p-2 sm:hidden cursor-pointer menu-icon" onClick={this.handleClick}><svg xmlns="http://www.w3.org/2000/svg" className="sm:hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <div className="text-black py-1 mx-2 sm:hidden cursor-pointer menu-icon" onClick={this.handleClick}><svg xmlns="http://www.w3.org/2000/svg" className="sm:hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
 </svg></div>
 <div>

2c3c979675a1940e0bc2882f83b0d766733cce5b
tejasc0 <tejasc888@gmail.com>
2021-09-24 09:19:37 +0000

    2 blogs added

--- pages/blog.js
+++ pages/blog.js
@@ -6,15 +6,52 @@ import Time_Ago from "../components/other/time-ago"
 const Blog = () => {
   return (
     <>
-              <Head>
+      <Head>
         <title>Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <div className="grid grid-cols-3">
     <div className="sm:col-span-2 divide-y-2 col-span-3">
+
     <div className="flex flex-1 m-8 justify-center">
         <div className="text-2xl font-semibold p-6 sm:w-2/3 w-full">
-            <p><Link href="/blogs/how-to-find-your-passion"><a> How to Find your Passion</a></Link></p>
+            <p><Link href="/blogs/tips-for-overcoming-procrastination"><a>Tips for Overcoming Procrastination</a></Link></p>
+            <p className="text-base font-light text-gray-800 mt-3"><Link href="/blogs/tips-for-overcoming-procrastination"><a>In our lives, we all have experienced a certain trait that sometimes makes us feel stuck in one place. For instance, when trying to complete...</a></Link></p>
+            <div className="flex flex-1">
+            <p className="text-sm font-normal text-gray-800 mt-3"><Time_Ago date="September 24 2021"/></p>
+            <div className="flex flex-wrap">
+        <Link href="/topics/self-help" passHref><p className="border-2 bg-gray-100 p-1 ml-4 text-sm font-medium rounded-full text-center cursor-pointer mt-2 px-auto">Self Help</p></Link>
+        </div>
+        </div>
+        </div>
+        <div className="hidden sm:block">
+        <Link href="/blogs/how-to-find-your-passion"><a>
+          <img className="h-32 w-48 m-6 rounded-lg" src="/blogs/cottonbro-6214649.jpg" alt="" />
+          </a></Link>
+        </div>
+        </div>
+
+    <div className="flex flex-1 m-8 justify-center">
+        <div className="text-2xl font-semibold p-6 sm:w-2/3 w-full">
+            <p><Link href="/blogs/gap-year-the-aftermath-of-highschool"><a>Gap Year: The Aftermath of Highschool</a></Link></p>
+            <p className="text-base font-light text-gray-800 mt-3"><Link href="/blogs/gap-year-the-aftermath-of-highschool"><a>Graduation is that time of your life where progression has started to come where new beginnings and the first chapter of your story would take...</a></Link></p>
+            <div className="flex flex-1">
+            <p className="text-sm font-normal text-gray-800 mt-3"><Time_Ago date="September 24 2021"/></p>
+            <div className="flex flex-wrap">
+        <Link href="/topics/career" passHref><p className="border-2 bg-gray-100 p-1 ml-4 text-sm font-medium rounded-full text-center cursor-pointer mt-2 px-auto">Career</p></Link>
+        </div>
+        </div>
+        </div>
+        <div className="hidden sm:block">
+        <Link href="/blogs/how-to-find-your-passion"><a>
+          <img className="h-32 w-48 m-6 rounded-lg" src="/blogs/rodnae-productions-7683763.jpg" alt="" />
+          </a></Link>
+        </div>
+        </div>
+
+    <div className="flex flex-1 m-8 justify-center">
+        <div className="text-2xl font-semibold p-6 sm:w-2/3 w-full">
+            <p><Link href="/blogs/how-to-find-your-passion"><a>How to Find your Passion</a></Link></p>
             <p className="text-base font-light text-gray-800 mt-3"><Link href="/blogs/how-to-find-your-passion"><a>Passion is a feeling of intense enthusiasm or desire for someone or something. Passion can vary from eager interest in or admiration for an idea...</a></Link></p>
             <div className="flex flex-1">
             <p className="text-sm font-normal text-gray-800 mt-3"><Time_Ago date="August 29 2021"/></p>
@@ -28,8 +65,6 @@ const Blog = () => {
           <img className="h-32 w-48 m-6 rounded-lg" src="/articles/pexels-burst-374068.jpg" alt="" />
           </a></Link>
         </div>
-        
-    
         </div>
         
         <div className="flex flex-1 m-8 justify-center">
---
+++ pages/blogs/tips-for-overcoming-procrastination.js
@@ -0,0 +1,36 @@
+import Blogpage_Layout from "../../components/layouts/blogpage-layout"
+import Blog_Layout from "../../components/layouts/blog-layout"
+import Link from "next/link"
+
+const Blog_9 = () => {
+  return (
+    <Blogpage_Layout>
+      <Blog_Layout
+      h1={<>Tips for Overcoming Procrastination</>}
+      username={<>Julean Palpallatoc</>}
+      image={<><img src="/authors/author-female.png" alt="Female Author" /></>}
+      p={<>In our lives, we all have experienced a certain trait that sometimes makes us feel stuck in one place. For instance, when trying to complete an assignment, you have the urge of possibly doing it later and just doing some other activity to keep your head out of it. This certain urge would be defined as procrastination, which delays a task to either put it off in a last-second or any other time before the deadline. Sometimes, procrastination could also be about persuading yourself to completely finish it off as it would be much beneficial for your time management. But the truth is, it is seemingly difficult to overcome it as there are loads of other activities to do instead of the initial task that you were supposed to do.<br /> <br />
+      Procrastination often comes around when you’re doing a school task or any sort of work-related assignment. You were assigned to finish it before the deadline when suddenly your mind has told you that such task could be finished later on. As the youth of our generation, we have certain distractions that comes in our way, especially the idea of social media platforms and more modern technological advancements that includes the virtual and non-virtual spaces. Besides the evolution, we sometimes lack self-discipline where we often have a hard time controlling ourselves into doing something else. This could either be the cause of the lack of motivation or the pressure that we put ourselves into. <br /> <br />
+      Sometimes, others identify procrastination as “laziness” which isn’t at all. Its mostly the desire of simply completing a task but could be difficult to maintain due to the time management skills or the causes that were mentioned above. As trying to balance your tasks, its also difficult to decide whether doing a task would be worth it, hence, you start to doubt that doing so would completely benefit you. This would be known as a decision paralysis where you often have a lot of freedom to make your decisions which could reflect your actions when doing your work. Procrastination is difficult to overcome as it ignores the value of time and the consequences you would get in the near future. <br /> <br />
+      Although procrastination will live a long in our lives, there will be some occassions where we would have to go through it in order to productively finish the tasks that was either given to us or simply to get it done right away. Hence, to slowly fight over procrastination, here are some tips to go over it: <br /> <br />
+      1. Time Management: As always, managing your time will benefit the situation by understanding the consequences that could happen if its done on time or not. Hence, try organizing a list or notes that could balance the tasks from a certain time period. For instance, organizing when and what time you should begin your tasks such as prioritizing it based on the significance and deadline it is. It is also important to take breaks in which you should keep in mind. <br /> <br />
+      2. Minizime Distractions: Having to limit the distractions would help you focus upon doing your tasks such as closing off social media platforms, streaming services, and other activities that would distract you. To slowly limit them, you can start of by shutting them down by replacing with an easy task to finish within an hour. <br /> <br />
+      3. Create a Planner: Although this is similar towards time management, creating a planner could possibly be planned a day, week, or month ahead in order to see what goes on during that time period. Thus, having planners such as a to-do list and a monthly planner would see the overall tasks that needs to be done within the deadline. You can either create a template for yourselves or use Canva, Goodnotes, and other Google templates to prepare so. <br /> <br />
+      4. Have a Reward: Having to remind yourselves to constantly finish your tasks would be hectic but rewarding yourself in the end of the day would be the finishing touch of your duties. Thus, this could motivate you to keep going and to focus on completing your productive day! <br /> <br />
+      5. Improve Your Routine: Lastly, improving your life style would benefit your productivity as it would create a healthy environment when trying to limit yourself from procrastination. Therefore, try sleeping early, eat properly, and prioritizing what needs to be done could help organize the essential activities. Planning this life style would not only improve your productivity but als your character. <br /> <br />
+      Procrastination may be a difficult obstacle to overcome, but at the end of the day, it is just an urge that wants you to keep away from doing the productive tasks to go on. <br /> <br />
+      <span className="font-semibold">References</span> <br />
+      Schicker A. <Link href="https://procrastination.com/what-is-procrastination" passHref><a className="text-blue-600 hover:underline">What Is PROCRASTINATION and How Can You Overcome It? </a></Link>Procrastination.com. Published February 5, 2013. Accessed September 9, 2021. <br />
+
+      <Link href="https://www.mindtools.com/pages/article/newHTE_96.htm" passHref><a className="text-blue-600 hover:underline">Procrastination.com. Published February 5, 2013. Accessed September 9, 2021. </a></Link>Mindtools.com. Published 2011. Accessed September 9, 2021. <br />
+      <Link href="https://www.spica.com/blog/how-to-stop-procrastinating" passHref><a className="text-blue-600 hover:underline">‌11 proven tips to stop procrastinating once and for all | Spica International.</a></Link>Spica.com. Published 2021. Accessed September 9, 2021. <br />
+      Corporate Finance Institute. 
+      <Link href="https://corporatefinanceinstitute.com/resources/careers/soft-skills/time-management-list-tips/" passHref><a className="text-blue-600 hover:underline">Time Management.</a></Link>Published May 21, 2019. Accessed September 9, 2021. 
+        </>}
+      />
+      
+    </Blogpage_Layout>
+  )
+}
+
+export default Blog_9

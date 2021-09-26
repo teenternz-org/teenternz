db9b327d0aba2c658c5af0d8228ef69b3e869aba
tejasc0 <tejasc888@gmail.com>
2021-09-26 11:14:36 +0000

    fixes

--- pages/events.js
+++ pages/events.js
@@ -14,7 +14,7 @@ const Events = () => {
     <img src="/interviews/i1.jpg" alt="" />
     </div>
     <p className="text-xl mt-4 mx-16">How 13 and 7 years old started their STEM podcast<span className="text-base"><Time_Ago date="3 September 2021" /></span><span className="text-base">Jaansi and Sunny Patel are siblings running LearnOn Podcast, the science show by kids, for kids!</span></p>
-    <a href="https://www.instagram.com/tv/CTXKTdJlS3D/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noreferrer"><button className="border-2 bg-gray-100 p-2 ml-16 text-sm font-medium rounded-full text-center cursor-pointer mt-6">Watch the recap</button></a>
+    <a href="https://www.instagram.com/tv/CTXKTdJlS3D/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noreferrer"><button className="border-2 bg-gray-100 p-1 ml-16 text-sm font-medium rounded-full text-center cursor-pointer mt-6">Watch</button></a>
     </>
   )
 }

bd08efb527e4639d06f7d27ecb87ffb074ce3e53
tejasc0 <tejasc888@gmail.com>
2021-10-14 10:50:15 +0000

    fixes

--- pages/internships.js
+++ pages/internships.js
@@ -33,7 +33,6 @@ export default function Internships({ internships, categories }) {
         	})}
 				</div>
         </div>
-        {/* 
 <div>
 <div className="hidden md:block -mt-24 fixed">
         <h1 className="text-2xl font-medium ml-8">
@@ -56,7 +55,7 @@ export default function Internships({ internships, categories }) {
         </div>
 </div>
 </div>
-        */}
+        
 
 </div>
     </>
@@ -67,13 +66,13 @@ export const getStaticProps = async () => {
   const response = await fetch('https://teenternz.online/api/internships')
   const data = await response.json()
   const internships = data
-  //const categoriesresponse = await fetch('https://teenternz.vercel.app/api/categories')
-  //const categories = await categoriesresponse.json()
+  const categoriesresponse = await fetch('https://teenternz.vercel.app/api/categories')
+  const categories = await categoriesresponse.json()
 
   return { 
     props: {
         internships,
-    //    categories
+        categories
   }
   }
 }

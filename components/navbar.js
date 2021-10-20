0c1b41f3ad5ac3067fa66d5f5939a99316b7b137
tejasc0 <tejasc888@gmail.com>
2021-10-20 04:32:53 +0000

    fixes

--- components/navbar.js
+++ components/navbar.js
@@ -46,18 +46,12 @@ const Navbar = () => {
         </div>
         <div className="sm:hidden cursor-pointer menu-icon" onClick={ active ? handleClose : handleClick}>
           { 
-          active ?
+          !active &&
           <div>
-<svg xmlns="http://www.w3.org/2000/svg" className="sm:hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
+</svg>
           </div>
-           :
-           <div>
-<svg xmlns="http://www.w3.org/2000/svg" className="sm:hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-          </svg>
-           </div>
            }
 </div>
         

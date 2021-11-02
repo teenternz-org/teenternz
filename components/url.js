15fca3f4e01970907a8cb8f19ff296b1032a6ccf
tejasc0 <tejasc888@gmail.com>
2021-11-02 10:53:54 +0000

    fixes

--- components/url.js
+++ components/url.js
@@ -1,4 +1,4 @@
 const devPreviewEnv = process.env.DEV_URL
 const prodEnv = process.env.PROD_URL
-const devEnv = devPreviewEnv !== 'http://127.0.0.1:3000' ? process.env.PREVIEW_URL : 'http://127.0.0.1:3000'
+const devEnv = devPreviewEnv !== 'http://127.0.0.1:3000' ? 'https://teenternz.vercel.app' : 'http://127.0.0.1:3000'
 export const server = devEnv;

4390633dde178496a7ca726bbc97a5ac7c052745
tejasc0 <tejasc888@gmail.com>
2021-11-02 11:13:11 +0000

    changed api endpoints to prod

--- components/url.js
+++ components/url.js
@@ -1,4 +1,4 @@
 const devPreviewEnv = process.env.DEV_URL
 const prodEnv = process.env.PROD_URL
 const devEnv = devPreviewEnv !== 'http://127.0.0.1:3000' ? 'https://teenternz.vercel.app' : 'http://127.0.0.1:3000'
-export const server = devEnv;
+export const server = prodEnv;
